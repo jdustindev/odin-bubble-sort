0f41cf2fcbd670cfa66d27e62904286c5bbcc2a8
jdustindev <jdustindev@gmail.com>
2022-12-19 16:13:00 -0500

    Implement bubble_sort()

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,19 @@
 def bubble_sort(numbers)
-    
+    new_numbers = numbers.dup
+    n = new_numbers.length
+    loop do
+        swapped = false
+        for i in 1..n-1
+            if new_numbers[i-1] > new_numbers[i]
+                new_numbers[i-1], new_numbers[i] = new_numbers[i], new_numbers[i-1]
+                swapped = true
+            end
+        end
+        break if !swapped
+    end
+    new_numbers
 end
 
-puts bubble_sort([4,3,78,2,0,2])
+numbers = [4,3,78,2,0,2]
+p bubble_sort(numbers)
+p numbers   # make sure original array is not altered
