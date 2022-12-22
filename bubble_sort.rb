1bd8acd75955988624a237f48fcd210cfbf7029e
jdustindev <jdustindev@gmail.com>
2022-12-22 12:23:33 -0500

    Made some style corrections (rubocop)

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,19 @@
 def bubble_sort(numbers)
-    new_numbers = numbers.dup
-    n = new_numbers.length
-    loop do
-        swapped = false
-        for i in 1..n-1
-            if new_numbers[i-1] > new_numbers[i]
-                new_numbers[i-1], new_numbers[i] = new_numbers[i], new_numbers[i-1]
-                swapped = true
-            end
-        end
-        break if !swapped
+  new_numbers = numbers.dup
+  n = new_numbers.length
+  loop do
+    swapped = false
+    for i in 1..n - 1
+      if new_numbers[i - 1] > new_numbers[i]
+        new_numbers[i - 1], new_numbers[i] = new_numbers[i], new_numbers[i - 1]
+        swapped = true
+      end
     end
-    new_numbers
+    break if !swapped
+  end
+  new_numbers
 end
 
-numbers = [4,3,78,2,0,2]
+numbers = [4, 3, 78, 2, 0, 2]
 p bubble_sort(numbers)
 p numbers   # make sure original array is not altered
